ae3b4d6514736f4710a35a4d67defc75a99e8098
bhakthi133 <bhakthi.sp@datagrokr.co>
2026-09-14 21:28:08 +0530

    Remove input handling from calculator.py
    Removed user input handling and operation execution.

--- calculator.py
+++ calculator.py
@@ -14,21 +14,4 @@ def square(n1):
 def power(n1,n2):
     return n1**n2
 
-n1=int(input("Enter 1st number: "))
-n2=int(input("Enter 2nd number: "))
-opp=input("Enter operation: ")
-if(opp == "add"):
-    res=add(n1,n2)
-elif(opp == "sub"):
-    res=sub(n1,n2)
-elif(opp=="mul"):
-    res=mul(n1,n2)
-elif(opp=="div"):
-    res=div(n1,n2)
-elif(opp=="square"):
-    res=square(n1)
-elif(opp=="power"):
-    res=power(n1,n2)
-else:
-    print("invalid")
-print("Result:", res)
+
