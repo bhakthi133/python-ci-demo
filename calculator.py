d4fd9cbdf513289c8f3141cdab010def593871ff
bhakthi <bhakthi.sp@datagrokr.co>
2026-09-16 10:36:09 +0530

    Docker files added

--- calculator.py
+++ calculator.py
@@ -14,4 +14,12 @@ def square(n1):
 def power(n1,n2):
     return n1**n2
 
-
+if __name__ == "__main__":
+    num1=int(input("ENter first number:"))
+    num2=int(input("Enter second number:"))
+    print("Addition:", add(num1, num2))
+    print("Subtraction:", sub(num1, num2))
+    print("Multiplication:", mul(num1, num2))
+    print("Division:", div(num1, num2))
+    print("Square of first number:", square(num1))
+    print("Power:", power(num1, num2))
